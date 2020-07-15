e7fd3a87dc9abec54119d195e10dd97bff3265fe
s-continuum, IT265 <noreply-jenkins@post.ch>
2020-07-15 15:26:22 +0200

    v1.1.13

---
+++ v1.1.13/model/ts/special_sort_table.ts
@@ -0,0 +1,29 @@
+import { Action } from "./sort_plan";
+import { SortingProduct } from "./sort_plan";
+
+export interface SpecialSortTable {
+    /**
+     * The date+timstamp which identifies the point in time, where the information was generated, including milliseconds.
+     * 
+     * @format date-time
+     */
+    timestamp: string;
+
+    /**
+     * The name of the special sort table
+     * Format: tablename.key.timestamp
+     * Example: SPECIALVZ.201446.20180222100409 
+     * @maxLength 40
+     */
+    name: string;
+
+    /**
+     * @minItems 1
+     */
+    sortingProducts: SortingProduct[];
+
+    /**
+     * @minItems 1
+     */
+    actions: Action[];
+}
